59f8ea0e8f21c5e33fce03ad192431230bf7c731
conmac22 <conmac22@vt.edu>
2021-11-14 20:28:45 -0500

    Upload

--- services.py
+++ services.py
@@ -1,22 +1,18 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
 from zeroconf import ServiceBrowser, Zeroconf
 from threading import Thread
 import serviceKeys
 import requests
 import pymongo
-
-ipAddress = ''
-port = ''
-ledColors = []
+import urllib.request
 
 # Zeroconf listener
 class ZeroconfListener:
     def add_service(self, zeroconf, type, name):
-        print("Connected")
+        global ipAddress, port, ledColors
         info = zeroconf.get_service_info(type, name)
-        ipAddress = info.parsed_addresses()
+        ipAddress = info.parsed_addresses()[0]
         port = info.port
         for (key, value) in info.properties.items():
             ledColors = (f'{value}')
@@ -29,28 +25,66 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 # Download Canvas file
-@app.route('/Canvas', methods=['GET'])
+@app.route('/Canvas', methods=['GET', 'POST'])
 @auth.login_required
-def download_file():
-    print(request.args)
+def canvas_file():
+    operation = request.args.get('operation')
     filename = request.args.get('file')
-    print(filename)
-    courseId = request.args.get('course_id')
-    print(courseId)
-    response = requests.get('https://vt.instructure/api/v1/courses/{}/files/?access_token={}&search_term={}'.format(courseId, serviceKeys.canvasToken, filename))
-    return response.json()
+    courseId = '136283'
+    
+    if operation == 'download':
+        response = requests.get('https://vt.instructure.com/api/v1/courses/{}/files/?access_token={}&search_term={}'.format(courseId, serviceKeys.canvasToken, filename))
+        urllib.request.urlretrieve(response.json()[0]['url'], filename)
+        return response.text
+    if operation == 'upload':
+        stepOneParams = {'name':filename}
+        firstResponse = requests.post('https://vt.instructure.com/api/v1/users/self/files/?access_token={}'.format(serviceKeys.canvasToken), data=stepOneParams)
+        print(firstResponse.text)
+        uploadUrl = firstResponse.json()['upload_url']
+        uploadParams = firstResponse.json()['upload_params']
+        uploadParams['file'] = '@'+filename
+        print(uploadParams)
+        secondResponse = requests.post(uploadUrl, data=uploadParams)
+        return secondResponse.text
+    
 
-# Control LED
-@app.route('/LED', methods = ['GET'])
+# Get LED status
+@app.route('/LED', methods = ['GET', 'POST'])
 @auth.login_required
-def control_led():
+def led():
+    global ipAddress, port, ledColors
     zeroconf = Zeroconf()
     listener = ZeroconfListener()
-    argsStr = 'status=on&color=magenta&intensity=80'
-    response = requests.get('http://'+ipAddress+':'+port+'/LED?' + argsStr)
+    browser = ServiceBrowser(zeroconf, '_http._tcp.local.', listener)
+    try:
+        input('Press enter to exit...\n')
+    finally:
+        zeroconf.close()
+    operation = request.args.get('operation')
+    if operation == 'get':
+        response = requests.get('http://'+str(ipAddress)+':'+str(port)+'/LED')
+        return response.text
+    if operation == 'post':
+        params = {'status':'on', 'color':'magenta', 'intensity':'80'}
+        response = requests.post('http://'+str(ipAddress)+':'+str(port)+'/LED', data=params)
+        return response.text
 
-def thread(zeroconf, string, listener):
-    browser = ServiceBrowser(zeroconf, '_http._tcp.local.', listener)    
+# # Get LED status
+# @app.route('/LED', methods=['POST'])
+# @auth.login_required
+# def post_led():
+#     global ipAddress, port, ledColors
+#     zeroconf = Zeroconf()
+#     listener = ZeroconfListener()
+#     browser = ServiceBrowser(zeroconf, '_http._tcp.local.', listener)
+#     try:
+#         input('Press enter to exit...\n')
+#     finally:
+#         zeroconf.close()
+#     params = {'status':'on', 'color':'magenta', 'intensity':'80'}
+#     response = requests.post('http://'+str(ipAddress)+':'+str(port)+'/LED?' + argsStr, data=params)
+#     return response.text
+
 
 # Authentication
 @auth.verify_password
@@ -73,4 +107,8 @@ def unauthorized():
 
 
 if __name__ == '__main__':
+    global ipAddress, port, ledColors
+    ipAddress = ''
+    port = ''
+    ledColors = []
     app.run(host='0.0.0.0')
